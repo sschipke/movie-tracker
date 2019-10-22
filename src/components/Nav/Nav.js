314fdf381f22fcb48cf863e84ab1654a38a80c8c
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-22 00:02:53 -0600

    Add initial styling to Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 import './Nav.css';
 import PropTypes from 'prop-types';
 
@@ -6,7 +7,24 @@ const Nav = () => {
 
 
   return (
-    'Nav Nav Nav'
+    <header>
+      <h1 className='header__logo'>Movie<span className='red'>Snag</span></h1>
+      <div className='header__nav'>
+        <NavLink exact to='/' className='NavLink' activeClassName="selectedLink" >  
+          Home
+        </NavLink>
+        <NavLink exact to='/top-movies' className='NavLink' activeClassName="selectedLink">  
+          Top Movies
+        </NavLink>
+        <NavLink exact to='/coming-soon' className='NavLink' activeClassName="selectedLink">  
+          Coming Soon
+        </NavLink>
+        <NavLink exact to='/favorites' className='NavLink' activeClassName="selectedLink">  
+          Favorites
+        </NavLink>
+      </div>
+      <div className='header__user-menu'>user menu with css</div>
+    </header>
   )
 
 }
