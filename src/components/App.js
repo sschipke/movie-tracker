84da77ecc4f149be54e816078117cdd5ef73bec9
Scott Schipke <swschipke@gmail.com>
2019-10-22 08:13:22 -0600

    Fetch first movies

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {getMovies} from '../apiCalls'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component { 
+  constructor() {
+    super()
+    this.state = {
+      movies: []
+    }
+  }
 
+  async componentDidMount() {
+    try {
+      const data = await getMovies();
+      this.setState({movies:data})
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import rootReducer from './reducers';
 import App from './components/App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, composeWithDevTools())
+// const store = createStore(rootReducer, composeWithDevTools())
 
 
 ReactDOM.render(
-  <Provider 
-    store= {store}
-  >
-    <App />
-  </Provider>,
+  // <Provider 
+  //   // store= {store}
+  // >
+    <App />,
+  // {/* </Provider>, */}
   document.getElementById('root')
 );
 
