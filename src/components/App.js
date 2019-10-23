07a34d389bb7aaff62bbe0e2094617ac46cb7fb3
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-23 14:38:16 -0600

    Change the way data was being passes to more closely track the initial wireframe

--- src/components/App.js
+++ src/components/App.js
@@ -6,27 +6,21 @@ import { Route } from 'react-router-dom';
 //Importing Other Componets
 import Login from './Login/Login';
 import Nav from './Nav/Nav';
-import MovieList from './MovieList/MovieList';
 import Favorites from './Favorites/Favorites';
+import Main from './Main/Main'
 import MoviePage from './MoviePage/MoviePage';
 import {getMovies} from '../util/apiCalls'
 import {setMovies} from '../actions'
 import './App.css';
 import { connect } from 'react-redux';
+
+
 class App extends Component { 
-  // We shouldn't need state now with redux
-  // constructor() {
-  //   super()
-  //   this.state = {
-  //     movies: []
-  //   }
-  // }
 
   async componentDidMount() {
     const {setMovies} = this.props;
     try {
       const data = await getMovies();
-      console.log(data)
       setMovies(data)
     } catch(error) {
       console.log(error)
@@ -38,7 +32,7 @@ class App extends Component {
       <div className="App">
         <Route exact path='/login' render={ (props)=> <Login {...props}/>} />
         <Route path='/' render={ () => <Nav /> } />
-        <Route exact path='/' render={ () => <MovieList /> } />
+        <Route exact path='/' render={ () => <Main /> } />
         <Route exact path='/favorites' render={ () => <Favorites /> } />
         <Route exact path='/movie/:id' render={ () => <MoviePage /> } />
       </div> 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,26 @@
-// import React from 'react';
-// import MovieList from '../MovieList/MovieList';
-// import './Main.css';
+import React from 'react';
+import MovieList from '../MovieList/MovieList';
+import { connect } from 'react-redux';
+import './Main.css';
 
 
-// const Main = () => {
+const Main = ({movies}) => {
+  
+  return (
+    <main>
+      <h1>Hot movies this week</h1>
+      <MovieList className = 'current-movies' movies={movies}/>
+      <h1>Upcomming Releases</h1>
+      {/* <MovieList className = 'upcoming-movies' movies={upcomingMovies} /> */}
+    </main>
+  )
+}
 
-//   return (
-//     <main>
-//       <h1>Hot movies this week</h1>
-//       <MovieList className = 'current-movies' />
-//       <h1>Upcomming Releases</h1>
-//       <MovieList className = 'upcoming-movies' />
-//     </main>
-//   )
-// }
 
-// export default Main;
+const mapStateToProps =  state => ({
+  movies: state.movies,
+  upcomingMovies: state.upcomingMovies
+})
+
+
+export default connect(mapStateToProps)(Main);
