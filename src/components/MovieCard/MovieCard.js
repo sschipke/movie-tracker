cfdbaa834c6dd0d96b65209194c5704ae33efba9
Scott Schipke <swschipke@gmail.com>
2019-10-23 15:29:05 -0600

    Style MovieCards to scroll horizontally

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,13 +5,13 @@ const MovieCard = ({movie}) => {
 
   return (
     <div className='movie'>
-      <div className='card__header'>
+      <h2 className='card__header'>
         {movie.title}
-      </div>
+      </h2>
       <img className='card_image' alt="movie poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} />
-      <div className='card__release-date'>
+      <h4 className='card__release-date'>
         {movie.release_date.slice(0,4)}
-      </div>
+      </h4>
     </div>
   )
 }
