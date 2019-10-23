e5e3f222d96e143bef3beb7f9f5bac2976bbde04
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-23 15:07:41 -0600

    Add the upcoming movies fetch

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -3,3 +3,10 @@ export const getMovies = async () => {
   let data = await res.json()
   return data.results
 }
+
+
+export const getUpcomingMovies = async () => {
+  let res = await fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=9e1950f701d0af712babdede31811e9e&language=en-US')
+  let data = await res.json()
+  return data.results
+}
