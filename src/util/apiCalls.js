2da5b5c5a50cbea5c22ffebac640cd6ce5255e6d
Scott Schipke <swschipke@gmail.com>
2019-10-23 14:10:07 -0600

    Change initial fetch url to get most current movies

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -1,5 +1,5 @@
 export const getMovies = async () => {
-  let res = await fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=9e1950f701d0af712babdede31811e9e')
+  let res = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=9e1950f701d0af712babdede31811e9e')
   let data = await res.json()
   return data.results
 }
