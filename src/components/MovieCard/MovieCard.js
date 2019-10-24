2ff275a451ca5416f188c96657abc9504ac64e97
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-23 22:17:36 -0600

    Center movie titles vertically on the MovieCard

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,9 +5,11 @@ const MovieCard = ({movie}) => {
 
   return (
     <div className='movie'>
-      <p className='card__header'>
-        {movie.title}
-      </p>
+      <div className='card__header'>
+        <p>
+          {movie.title}
+        </p>
+      </div>
       <img className='card_image' alt="movie poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} />
       <h4 className='card__release-date'>
         {movie.release_date.slice(0,4)}
