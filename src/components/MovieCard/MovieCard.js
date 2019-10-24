47f03ad35b3c875ac965de5656e6ccc13da94ce4
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-23 23:51:22 -0600

    Add star image to MovieCard for favoriting

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './MovieCard.css';
+import star from '../../images/bluestar.svg';
+import favstar from '../../images/fav-star.svg'
 
 const MovieCard = ({movie}) => {
 
   return (
     <div className='movie'>
-      <img className='card_image' alt="movie poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} />
+      <img alt='star indicating is a movie is favorited or not' src={star} className='card__image-favorite' />
+      <img className='card__image' alt="movie poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} />
       <div className='card__header'>
         <p>
           {movie.title}
