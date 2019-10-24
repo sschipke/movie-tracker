857404cb18dd4753c3d022735c579dfbb0910b63
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-10-23 22:56:54 -0600

    Move movies poster to the top of MovieCard

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,12 +5,12 @@ const MovieCard = ({movie}) => {
 
   return (
     <div className='movie'>
+      <img className='card_image' alt="movie poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} />
       <div className='card__header'>
         <p>
           {movie.title}
         </p>
       </div>
-      <img className='card_image' alt="movie poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} />
       <h4 className='card__release-date'>
         {movie.release_date.slice(0,4)}
       </h4>
