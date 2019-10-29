6e2e1c22b6bf4a4b783a48e1a6eab24216abfdb0
Scott Schipke <swschipke@gmail.com>
2019-10-28 20:22:56 -0600

    Add tests for getUserFavorites fetch

--- src/util/apiCalls.test.js
+++ src/util/apiCalls.test.js
@@ -189,4 +189,49 @@ describe('logInUser', () => {
   })
 })
 
+describe('getUserFavorites', () => {
+  let mockFaves = [{
+    user_id: 5,
+    "poster_path": "/pIcV8XXIIvJCbtPoxF9qHMKdRr2.jpg",
+    "id": 338967,
+    "title": "Zombieland: Double Tap",
+    "vote_average": 7.3,
+    "overview": "The group will face a new zombie threat as a new breed of zombie has developed. This new super-zombie type is faster, bigger, and stronger than the previous strain of zombies and harder to kill. These super-zombies have started grouping up into a horde going from city to city leaving a path of destruction behind them.",
+    "release_date": "2019-10-18"
+  },
+  {
+    user_id: 5,
+    "poster_path": "/vqzNJRH4YyquRiWxCCOH0aXggHI.jpg",
+    "id": 290859,
+    "title": "Terminator: Dark Fate",
+    "vote_average": 7.4,
+    "overview": "More than two decades have passed since Sarah Connor prevented Judgment Day, changed the future, and re-wrote the fate of the human race. Dani Ramos is living a simple life in Mexico City with her brother and father when a highly advanced and deadly new Terminator – a Rev-9 – travels back through time to hunt and kill her. Dani's survival depends on her joining forces with two warriors: Grace, an enhanced super-soldier from the future, and a battle-hardened Sarah Connor. As the Rev-9 ruthlessly destroys everything and everyone in its path on the hunt for Dani, the three are led to a T-800 from Sarah’s past that may be their last best hope.",
+    "release_date": "2019-11-01"
+  }]
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve({favorites:mockFaves})
+      })
+    })
+  });
+  it('should be called with the correct arguments', () => {
+    const expected = 'http://localhost:3001/api/v1/users/5/moviefavorites'
+    getUserFavorites(5);
+    expect(window.fetch).toHaveBeenCalledWith(expected)
+  });
+  it('should return an array of favorites (HAPPY)', () => {
+    expect(getUserFavorites(5)).resolves.toEqual(mockFaves);
+  });
+  it('should throw an error if something goes wrong (SAD)', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false
+      })
+    })
+    expect(getUserFavorites(4)).rejects.toEqual(Error('Unable to get favorites'));
+  })
+})
+
 
